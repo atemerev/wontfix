4e998258355a7df5f92dbc18a7b52213a17c07db
Alexander Temerev <sorhed@gmail.com>
2011-07-25 22:01:15 +0200

    Type parsing (nearly) works.

--- src/main/scala/codec.scala
+++ src/main/scala/codec.scala
@@ -22,8 +22,11 @@ package com.miriamlaurel.wontfix.codec
 import com.miriamlaurel.wontfix.util._
 import com.miriamlaurel.wontfix.types._
 import java.nio.charset.Charset
+import xml.Elem
+import com.miriamlaurel.wontfix.numbers.Decimal
+import java.util.Date
 
-case class ParseError(message: String)
+case class ParseError(message: String) extends RuntimeException(message)
 
 trait ByteCodec {
   def encode(message: FixMessage): Array[Byte]
@@ -31,15 +34,51 @@ trait ByteCodec {
   def incrementalDecode(data: Array[Byte]): Seq[Either[FixMessage, ParseError]] = undefined
 }
 
-class FixtpCodec(val version: String) extends ByteCodec {
+class FixtpCodec(val version: String, dictionary: Elem) extends ByteCodec {
+
+  val fields = dictionary \\ "fields" \ "field"
+  val typeMap = Map(fields.map(f => ((f \ "@number").text.toInt -> ((f \ "@type").text match {
+    case "STRING" => FixString(_: String)
+    case "CHAR" => v: String => FixChar(v.toCharArray()(0))
+    case "INT" => v: String => FixInteger(v.toInt)
+    case "FLOAT" => v: String => FixFloat(Decimal(v))
+    case "LENGTH" => v: String => Length(v.toInt)
+    case "SEQNUM" => v: String => SeqNum(v.toInt)
+    case "NUMINGROUP" => v: String => NumInGroup(v.toInt)
+    case "DAY-OF-MONTH" => v: String => DayOfMonth(v.toInt)
+    case "QTY" => v: String => Qty(Decimal(v))
+    case "PRICE" => v: String => Price(Decimal(v))
+    case "PRICEOFFSET" => v: String => PriceOffset(Decimal(v))
+    case "AMT" => v: String => Amt(Decimal(v))
+    case "PERCENTAGE" => v: String => Percentage(Decimal(v))
+    case "BOOLEAN" => v: String => FixBoolean(if (v == "Y") true
+      else if (v == "N") false
+      else throw new ParseError("Unknown boolean value: " + v))
+    case "MULTIPLECHARVALUE" => v: String => MultipleCharValue(v.split(" ").map(_.toCharArray()(0)): _*)
+    case "MULTIPLESTRINGVALUE" => v: String => MultipleStringValue(v.split(" "): _*)
+    case "COUNTRY" => v: String => Country(v)
+    case "CURRENCY" => v: String => Currency(v)
+    case "EXCHANGE" => v: String => Exchange(v)
+    case "MONTH-YEAR" => v: String => MonthYear(0, 1, None, None) // todo fix this
+    case "UTCTIMESTAMP" => v: String => UTCTimestamp(new Date) // todo fix this
+    case "UTCTIMEONLY" => v: String => UTCTimeOnly(0, 0, 0, 0) // todo fix this
+    case "LOCALMKTDATE" => v: String => LocalMktDate(0, 1, 1) // todo fix this
+    case "TZTIMEONLY" => v: String => TZTimeOnly(0, 0, 0, UTCUtil.UTC) // todo fix this
+    case "TZTIMESTAMP" => v: String => TZTimestamp(new Date, UTCUtil.UTC) // todo fix this
+    case "DATA" => v: String => Data(v.getBytes(UTF8))
+    case _ => throw new ParseError("Unknown type")
+  }))): _*)
 
   private val UTF8 = Charset.forName("UTF-8")
+
+  /*! Since we want something working first, we will not bother with SeqNum and SenderCompID at
+      this time. */
 
   def encode(message: FixMessage) = {
     val body = message.structure.flatten
     val typeField = FixField(35, message.msgType)
     val begin = FixField(8, version)
-    val bodyData = body.map(fieldToBytes(_)).foldLeft(Array[Byte]())(_ ++ _)
+    val bodyData = Array.concat(body.map(fieldToBytes(_)): _*)
     val bodyLength = FixField(9, bodyData.length)
     val checksum = FixField(10, bodyData.foldLeft(0)(_.toInt + _.toInt) % 256)
     Array.concat(
@@ -50,7 +89,22 @@ class FixtpCodec(val version: String) extends ByteCodec {
       fieldToBytes(checksum)
     )
   }
-  def decode(data: Array[Byte]) = undefined
+
+  def decode(data: Array[Byte]) = {
+    val tokens = (new String(data, UTF8)).split(1.toChar).toList
+    if (tokens.size < 3) Right(ParseError("FIX message should contain at least 3 fields")) else tokens match {
+      case beginString :: bodyLength :: rest => {
+        val checksumToken = rest.last
+      }
+      case _ => undefined
+    }
+    undefined
+  }
 
   private def fieldToBytes(field: FixField): Array[Byte] = field.toString.getBytes(UTF8) :+ 1.toByte
+
+  private def bytesToField(bytes: Array[Byte]): Option[FixField] = {
+    val tokens = new String(bytes, UTF8).split("=")
+    undefined
+  }
 }
